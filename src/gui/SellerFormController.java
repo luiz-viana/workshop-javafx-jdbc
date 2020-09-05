857e5f0e860849943831fddbf5b0818ea8e32155
luiz-viana <luiz9505@hotmail.com>
2020-09-05 14:09:40 -0300

    Final Version

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,10 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -100,8 +102,10 @@ public class SellerFormController implements Initializable {
 			Utils.currentStage(event).close();
 		} catch (ValidationException e) {
 			setErrorMessages(e.getErrors());
+			e.printStackTrace();
 		} catch (DbException e) {
-			Alerts.showAlert("Error saving objext", null, e.getMessage(), AlertType.ERROR);
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+			e.printStackTrace();
 		}
 	}
 
@@ -114,16 +118,38 @@ public class SellerFormController implements Initializable {
 
 	private Seller getFormData() {
 		Seller obj = new Seller();
-
 		ValidationException exception = new ValidationException("Validation Exception");
-
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
-		obj.setName(txtName.getText());
-
+		
 		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
 			exception.addError("name", "Field can't be empty");
 		}
+		obj.setName(txtName.getText());
+		
 
+		if (txtEmail.getText() == null || txtEmail.getText().trim().equals("")) {
+			exception.addError("email", "Field can't be empty");
+		}
+		obj.setEmail(txtEmail.getText());
+		
+		
+		if(dpBirthDate.getValue() == null) {
+			exception.addError("birthDate", "Field can't be empty");
+		}
+		else {
+			Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()));
+			obj.setBirthDate(Date.from(instant));
+		}
+		
+			
+		if (txtBaseSalary.getText() == null || txtBaseSalary.getText().trim().equals("")) {
+			exception.addError("baseSalary", "Field can't be empty");
+		}
+		obj.setBaseSalary(Utils.tryParseToDouble(txtBaseSalary.getText()));
+		
+		obj.setDepartment(comboBoxDepartment.getValue());
+		
+		
 		if (exception.getErrors().size() > 0) {
 			throw exception;
 		}
@@ -200,6 +226,33 @@ public class SellerFormController implements Initializable {
 
 		if (fields.contains("name")) {
 			labelErrorName.setText(errors.get("name"));
+		}
+		else {
+			labelErrorName.setText("");
+		}
+		
+		
+		if (fields.contains("email")) {
+			labelErrorEmail.setText(errors.get("email"));
+		}
+		else {
+			labelErrorEmail.setText("");
+		}
+		
+		
+		if (fields.contains("baseSalary")) {
+			labelErrorBaseSalary.setText(errors.get("baseSalary"));
+		}
+		else {
+			labelErrorBaseSalary.setText("");
+		}
+		
+		
+		if (fields.contains("birthDate")) {
+			labelErrorBirthDate.setText(errors.get("birthDate"));
+		}
+		else {
+			labelErrorBirthDate.setText("");
 		}
 	}
 
--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -128,7 +128,7 @@ public class SellerListController implements Initializable, DataChangeListener {
 			dialogStage.showAndWait();
 		} catch (IOException e) {
 			e.printStackTrace();
-			Alerts.showAlert("IOExceptio", "Error loading view", e.getMessage(), AlertType.ERROR);
+			Alerts.showAlert("IOException", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
 	}
 
@@ -152,8 +152,7 @@ public class SellerListController implements Initializable, DataChangeListener {
 					return;
 				}
 				setGraphic(button);
-				button.setOnAction(
-						event -> createDialogForm(obj, "/gui/SellerForm.fxml", gui.util.Utils.currentStage(event)));
+				button.setOnAction(event -> createDialogForm(obj, "/gui/SellerForm.fxml", gui.util.Utils.currentStage(event)));
 			}
 		});
 	}
